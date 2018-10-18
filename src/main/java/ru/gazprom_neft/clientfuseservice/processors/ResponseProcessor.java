8681a6dcb9c33a8d4e83e046e1a25dc77b9fc535
serzh <sltelitsyn@gmail.com>
2018-10-18 11:12:36 +0300

    logger isn't working, trying to debug code

--- src/main/java/ru/gazprom_neft/clientfuseservice/processors/ResponseProcessor.java
+++ src/main/java/ru/gazprom_neft/clientfuseservice/processors/ResponseProcessor.java
@@ -26,23 +26,30 @@ public class ResponseProcessor implements Processor {
     @Override
     public void process(Exchange exchange) throws Exception {
         LOGGER.debug("Entered responseProcessor");
+        System.out.println("Entered responseProcessor");
         String path = exchange.getIn().getHeader("CamelHttpPath", String.class);
         LOGGER.debug("Got correlationId: " + path);
+        System.out.println("Got correlationId: " + path);
         QueueConnection connection = amqConnectionFactory.createQueueConnection();
         QueueSession session = connection.createQueueSession(false, Session.AUTO_ACKNOWLEDGE);
         QueueReceiver receiver = session.createReceiver(new ActiveMQQueue("responses"), "JMSCorrelationID='" + path + "'");
         exchange.getOut().setHeader("JMSCorrelationID", path);
         connection.start();
+        System.out.println("Opened connection");
         try{
             ActiveMQMessage message = (ActiveMQMessage) receiver.receive();
+            System.out.println("message = "+message);
             String body = Arrays.toString(message.getContent().getData());
+            System.out.println("message boyd: "+ body);
             LOGGER.debug(String.format("Got message with id = %s and body = %s", message.getJMSMessageID(), body));
             exchange.getOut().setBody(body);
         } catch (JMSException ex) {
             LOGGER.error("Error while obtaining message: " + ex.getMessage());
+            System.out.println("Error while obtaining message: " + ex.getMessage());
             exchange.getOut().setBody("Error while obtaining message");
         }
         connection.stop();
+        System.out.println("Stopped connection");
     }
 
 }
